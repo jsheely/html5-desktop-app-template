426fd2ce33e5d2d310112f790dfd81b97b94448f
jsheely <jsheely@twentytech.net>
2015-11-28 17:12:53 -0500

    switched to using gulp instead of npm scripts

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp');
+var useref = require('gulp-useref');
+var gulpif = require('gulp-if');
+var less = require('gulp-less');
+var clean = require('gulp-clean');
+var watch = require('gulp-watch');
+var plumber = require('gulp-plumber');
+var webserver = require('gulp-webserver');
+var seq = require('run-sequence');
+
+gulp.task('clean', () => {
+	return gulp.src('build', { read: false })
+        .pipe(clean());
+});
+
+gulp.task('useref', (cb) => {
+	return gulp.src('./src/index.html')
+		.pipe(plumber({
+			errorHandler: function (err) {
+				console.log(err);
+				this.emit('end');
+			}
+		}))
+		.pipe(useref())
+		.pipe(gulpif('*.css', less()))
+		.pipe(gulp.dest('./build'));
+});
+
+
+gulp.task('build', (cb) => {
+	seq('clean','useref', cb);
+});
+
+gulp.task('start', (cb) => {
+	return gulp.src('build')
+		.pipe(webserver({
+			livereload: true,
+			open: true
+		}))
+});
+
+gulp.task('watch', ['start', 'build'], (cb) => {
+	watch('src/**/*.*', (f) => {
+		console.log('File changed: ', f.relative);
+		gulp.start('build');
+	});
+});
+
+gulp.task('default', ['build']);
